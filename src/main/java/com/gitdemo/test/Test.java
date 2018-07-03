b3b943634c62b1adfbdc8c06283caa23a159c4c5
w3cschool <w3c@w3cschool.cn>
2018-07-03 11:53:54 +0800

    first edit

--- src/main/java/com/gitdemo/test/Test.java
+++ src/main/java/com/gitdemo/test/Test.java
@@ -3,5 +3,6 @@ package com.gitdemo.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("I'm Git");
+        System.out.println("Someone edit me and commit!");
     }
 }
